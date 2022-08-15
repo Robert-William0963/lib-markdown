db71bf7d22c640eaa2dd5997c38a41c719a7105d
Robert-William0963 <robertwilliam0963@gmail.com>
2022-08-15 15:08:12 -0300

    lib-md

---
+++ teste/index.test.js
@@ -0,0 +1,8 @@
+const pegaArquivo = require('../index');
+
+describe('pegaArquivo::', () => {
+    it('deve ser uma função', ()=>{
+        expect(typeof pegaArquivo).toBe('function');
+    })
+})
+
